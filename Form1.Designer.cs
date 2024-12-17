64e287d88b5bd4ed8f8beece796441c8d4c1cbfb
Алексей Леонтьев <leontev.leha06@bk.ru>
2024-12-17 11:57:56 +0500

    создал форму и разместил информацию о партнеров

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,11 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
             panelTop = new Panel();
+            panelFill = new Panel();
+            panelTop.SuspendLayout();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Dock = DockStyle.Fill;
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label1.Location = new Point(10, 10);
+            label1.Name = "label1";
+            label1.Padding = new Padding(10);
+            label1.Size = new Size(780, 80);
+            label1.TabIndex = 0;
+            label1.Text = "Список партнеров:";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // panelTop
             // 
+            panelTop.Controls.Add(label1);
             panelTop.Dock = DockStyle.Top;
             panelTop.Location = new Point(0, 0);
             panelTop.Name = "panelTop";
@@ -40,19 +56,32 @@
             panelTop.Size = new Size(800, 100);
             panelTop.TabIndex = 0;
             // 
+            // panelFill
+            // 
+            panelFill.Dock = DockStyle.Fill;
+            panelFill.Location = new Point(0, 100);
+            panelFill.Name = "panelFill";
+            panelFill.Size = new Size(800, 350);
+            panelFill.TabIndex = 1;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(panelFill);
             Controls.Add(panelTop);
             Name = "Form1";
             Text = "FormMain";
+            Load += Form1_Load;
+            panelTop.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
+        private Label label1;
         private Panel panelTop;
+        private Panel panelFill;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace PR4
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (PartnersContext db = new PartnersContext())
+            {
+                var partners = db.Partners.Include(u=>u.IdTypeOfPartnerNavigation).ToList();
+                int yOffset = 10;
+                foreach (Partner u in partners)
+                {
+                    Panel panelPartn = new Panel
+                    {
+                        AutoSize = true,
+                       Location = new System.Drawing.Point(15, yOffset),
+                        BorderStyle = BorderStyle.FixedSingle,
+                    };
+
+
+                    Label partnerLabel = new Label
+                    {
+                        Text = $"{u.IdTypeOfPartnerNavigation.TypeOfPartner1}| " +
+                       $"{u.NameProduct}\n" +
+                       $"{u.DirectorsName}\n" +
+                       $"{u.MobilePhone}\n"+
+                       $"Рейтинг: {u.Rating}",
+                       AutoSize= true,
+
+                    };
+
+                   panelPartn.Controls.Add( partnerLabel);
+                    panelFill.Controls.Add( panelPartn );
+                    yOffset += panelPartn.Height + 15;
+                }
+            }
+ 
         }
     }
 }
